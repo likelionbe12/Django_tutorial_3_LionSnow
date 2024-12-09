7a61fc78a1f22ec35d1a97faf96d15c9f7c367f1
Im Jonghan <withyou@empal.com>
2024-12-09 10:45:07 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT= '/static/'
+STATIC_ROOT= BASE_DIR/'static'
 STATICFILES_DIRS = [BASE_DIR/'config/static']
 
 # media 파일 관련 세팅
